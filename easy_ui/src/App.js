a555a9824c0fe7f99394f9be4c12b82b8da86d6e
Nikhil Anantharaman <nikhilanantharaman@gmail.com>
2020-04-05 18:40:25 +0530

    Added pagination and server side search

--- easy_ui/src/App.js
+++ easy_ui/src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css'
 import imdbAPIKey from './keys';
 
-const DEFAULT_QUERY = 'Batman';
+const DEFAULT_QUERY = '';
 
 const PATH_BASE = 'http://omdbapi.com/'
 const API_KEY = `apikey=${imdbAPIKey.imdbAPIKey}`
-
+const PARAM_PAGE = 'page='
+const PARAM_SEARCH = 's='
 var url = `${PATH_BASE}?${API_KEY}&s=${DEFAULT_QUERY}`;
-
-const isSearched = (searchTerm) => (item) =>  item.Title.toLowerCase().includes(searchTerm.toLowerCase());
 
 const Button = ({onClick, className = '', children}) => {
     return(
@@ -21,65 +20,112 @@ const Button = ({onClick, className = '', children}) => {
     )
 }
 
-const Search =({value, onChange, children}) => {
+const Search =({value, onChange, onSubmit,children }) => {
     return (
-        <form>
-            {children} <input 
+        <form onSubmit = {onSubmit}>
+            <input 
                 type = "text" 
                 value = {value}
-                onChange = {onChange}/>
+                onChange = {onChange}
+                />
+                <button type = "submit">
+                    {children}
+                </button>
         </form>
     )    
 }
 
-const Table = ({list, pattern, onDismiss}) => {
+const Table = ({list, onDismiss}) => {
     const imageColumnWidth = {width : '20%'};
     const nameColumnWidth = { width: '20%'};
     const yearColumnWidth = { width: '30%'};
     const typeColumnWidth = { width: '20%'};
     const buttonColumnWidth = { width: '10%'};
-    if(!list) return null
-    return (
-        <div className = "table">
-            {list.filter(isSearched(pattern)).map(item =>    
-                <div key ={item.imdbID} className="table-row">
-                    <span style = {imageColumnWidth}><img src={item.Poster} height="100"></img></span>
-                    <span style = {nameColumnWidth}><a href={item.firstName}>{item.Title}</a></span>
-                    <span style = {yearColumnWidth}>{item.Year}</span>
-                    <span style = {typeColumnWidth}>{item.Type}</span>
-                    <span style = {buttonColumnWidth}> 
-                        <Button className = "button-inline"
-                            onClick={()=> onDismiss(item.imdbID)}
-                        >
-                        Dismiss
-                        </Button>
-                    </span>
-                </div>                                    
-            )}
-        </div>
-    )       
+     if(!list || list.Error) {
+        return <p>Please enter a valid title</p> 
+    } else {
+        return (
+            <div className = "table">
+                {list.map(item =>    
+                    <div key ={item.imdbID} className="table-row">
+                        <span style = {imageColumnWidth}><img src={item.Poster} height="100"></img></span>
+                        <span style = {nameColumnWidth}><a href={"https://www.imdb.com/title/" + item.imdbID} target="_blank" rel="noopener noreferrer">{item.Title}</a></span>
+                        <span style = {yearColumnWidth}>{item.Year}</span>
+                        <span style = {typeColumnWidth}>{item.Type}</span>
+                        <span style = {buttonColumnWidth}> 
+                            <Button className = "button-inline"
+                                onClick={()=> onDismiss(item.imdbID)}
+                            >
+                            Dismiss
+                            </Button>
+                        </span>
+                    </div>                                    
+                )}
+            </div>
+        )
+    }       
 }
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            result : null,
-            searchTerm : DEFAULT_QUERY
+            results : null,
+            searchKey : '',
+            searchTerm : DEFAULT_QUERY,
+            error : null
         }
     }
 
-    setSearchMovies = (result) => {
-        this.setState({result : result.result});
+    needsToSearch = (searchTerm) => {
+        if(this.state.results)  return !this.state.results[searchTerm];
+        else return true
+    }
+
+    setSearchMoviesorTVShows = (result) => {
+        const listOfContent = result.Search;
+        const {page}  =  result;
+        const {searchKey, results} = this.state;
+        const oldListOfContent = results && results[searchKey] ? results[searchKey].Search : [];
+        const updatedListOfContent = [ ...oldListOfContent, ...listOfContent];
+        const finalRes = {results : { ...results, [searchKey] : {Search: updatedListOfContent, page} }};
+        this.setState(finalRes);
     }
 
     onDismiss = (id) => {
+        const { searchKey, results} = this.state;
+        const {Search, page } = results[searchKey];
         const isNotId = item => item.imdbID !== id;
-        const updatedSearch = this.state.result.Search.filter(isNotId);
+        const updatedSearch = Search.filter(isNotId);
         this.setState({
-            result : { ...this.state.result,Search: updatedSearch },
-        })
-        //this.setState({result: Object.assign({},this.state.result,{Search: updatedList})});    
+            results : { ...results,[searchKey] : {Search : updatedSearch, page} },
+        })  
+    }
+
+    onSearchSubmit = (event) => {
+        event.preventDefault();
+        const { searchTerm } = this.state;
+
+        this.setState({searchKey : searchTerm});
+        if(this.needsToSearch(searchTerm)) {
+            this.searchMoviesOrTVShows(searchTerm);
+        }
+    }
+
+
+    searchMoviesOrTVShows = (searchTerm, page = 1) => {
+        if(searchTerm.length === 0) {
+            this.setState({ error : 'Please enter a movie'})
+        } else {
+            url = `${PATH_BASE}?${API_KEY}&${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`
+            fetch(url)
+            .then(response => response.json())
+            .then(result => {
+                let editedResult = {...result, page : page };
+                this.setSearchMoviesorTVShows(editedResult);
+            })
+            .catch(error => this.setState({ error }));
+        }  
     }
 
     onSearchChange = (event) => {
@@ -88,11 +134,8 @@ class App extends React.Component {
 
     componentDidMount() {
         const {searchTerm} = this.state;
-        url = `${PATH_BASE}?${API_KEY}&s=${searchTerm}`
-        fetch(url)
-        .then(response => response.json())
-        .then(result => this.setSearchMovies({result}))
-        .catch(error => error);
+        this.setState({searchKey : searchTerm});
+        this.searchMoviesOrTVShows(searchTerm);
     }
 
     componentDidCatch(err,info) {
@@ -101,40 +144,50 @@ class App extends React.Component {
     }
 
     render() {
-        console.log('state',this.state)
-        const { searchTerm, result } = this.state;
-        if( !result ) {
-            return (
-                <div className = "page">
-                    <div className = "interactions">
-                        <Search
-                        value={searchTerm}
-                        onChange= {this.onSearchChange}
-                            >Search
-                        </Search>
-                    </div>
-                </div>
-                    
-            )
-        } else {
+        const { 
+            searchTerm, 
+            results,
+            searchKey, 
+            error 
+        } = this.state;
+        if(error && error === 'Please enter a movie') {
             return(
-                <div className = "page">
-                    <div className = "interactions">
-                        <Search
-                        value={searchTerm}
-                        onChange= {this.onSearchChange}
-                            >Search
-                        </Search>
-                        <Table
-                        {...console.log('here', result)}
-                        list={result.Search}
-                        pattern={searchTerm}
-                        onDismiss={this.onDismiss}
-                        />
-                    </div>
+                <div>
+                    <Search
+                    value={searchTerm}
+                    onChange= {this.onSearchChange}
+                    onSubmit = {this.onSearchSubmit}
+                        >Search
+                    </Search>
+                    <p>Something went wrong</p>
                 </div>
-            )
+                )
+            
         }
+        const page = (results && results[searchKey] && results[searchKey].page) || 0;
+        const list = (results && results[searchKey] && results[searchKey].Search) || [];
+        return (
+            <div className = "page">
+                <div className = "interactions">
+                    <Search
+                    value={searchTerm}
+                    onChange= {this.onSearchChange}
+                    onSubmit = {this.onSearchSubmit}
+                        >Search
+                    </Search>
+                    <Table
+                        list={list}
+                        onDismiss={this.onDismiss}
+                    />
+                    
+                     <div className="interactions">
+                        <Button onClick={() => this.searchMoviesOrTVShows(searchKey, page+1 )}>
+                            More
+                        </Button>
+                    </div>  
+                </div>
+            </div>
+        )
     }
 }
 
