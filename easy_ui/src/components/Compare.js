6366e54a35e8e560d2270207c1bd18a9424b4b94
Nikhil Anantharaman <nikhilanantharaman@gmail.com>
2020-05-03 19:23:31 +0530

    Changed CSS for Compare

--- easy_ui/src/components/Compare.js
+++ easy_ui/src/components/Compare.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import './Compare.css';
 import SearchForCompare from './SearchForCompare';
-import ClipLoader from "react-spinners/ClipLoader";
-
-const Loading = () => {
-    return (
-        <div className="sweet-loading">
-          <ClipLoader/>
-        </div>
-    );
-}
-
-const withLoading = (Component) => ({isLoading, ...rest}) => {
-    return (
-        isLoading? 
-        <Loading />
-        :<Component {...rest}/>
-    )
-}
 
 class Compare extends React.Component {
     render () {
--- easy_ui/src/components/Search.js
+++ easy_ui/src/components/Search.js
@@ -2,35 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Search extends React.Component {
-    componentDidMount() {
-      if (this.input) {
-        this.input.focus();
-      }
-    }
-  
-    render() {
-      const {
-        value,
-        onChange,
-        onSubmit,
-        children
-      } = this.props;
-  
-      return (
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            value={value}
-            onChange={onChange}
-            ref={el => this.input = el}
-          />
-          <button type="submit">
-            {children}
-          </button>
-        </form>
-      );
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
     }
   }
+
+  render() {
+    const {
+      value,
+      onChange,
+      onSubmit,
+      children
+    } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={el => this.input = el}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    );
+  }
+}
 
 Search.propTypes = {
     value : PropTypes.string,
